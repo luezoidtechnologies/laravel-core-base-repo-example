c64af043517558d2e9bfdcdf20ab908fff5d2c77
Abhishek Mishra <abhishek@luezoid.com>
2020-04-25 01:21:49 +0530

    Example endpoints & working flow for /minions resource route

--- routes/api.php
+++ routes/api.php
@@ -14,3 +14,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::post('authenticate', 'AuthenticationController@doLogin')->name('authenticate.store');
+Route::resource('minions', 'MinionController', ['parameters' => ['minions' => 'id']]);
